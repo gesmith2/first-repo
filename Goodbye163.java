8401a2c8bed8b64c0d78c6dce9f8a1eda330b8da
gesmith2 <45207500+gesmith2@users.noreply.github.com>
2018-11-20 11:11:48 -0500

    Update Goodbye163.java

---
+++ Goodbye163.java
@@ -0,0 +1,16 @@
+/**
+ *This code just prints "Goodbye COMP 163".
+ * @author gesmith2@aggies.ncat.edu
+ */
+ 
+public class Goodbye163 {
+
+/**
+ * This method actually prints out the string.
+ * @param args tokens from the command line
+ */
+
+  public static void main(String[] args){
+    System.out.println("Goodbye COMP 163"); 
+  }
+}
